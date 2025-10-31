340158a193702956687afcd8f2565205645d7470
Wyc12-26 <3122819580@qq.com>
2025-10-31 10:51:34 +0800

    Add files via upload

---
+++ weiyincan2123044139/src/main/java/edu/fdzc/weiyincan212304139/Weiyincan2123044139Application.java
@@ -0,0 +1,13 @@
+package edu.fdzc.weiyincan212304139;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weiyincan2123044139Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Weiyincan2123044139Application.class, args);
+    }
+
+}
---
+++ weiyincan2123044139/src/test/java/edu/fdzc/weiyincan212304139/Weiyincan2123044139ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fdzc.weiyincan212304139;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Weiyincan2123044139ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
